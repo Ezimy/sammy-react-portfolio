a4c40e6e478ffcfecdb32793ed2d626bee9cc8ba
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-05-01 21:26:33 -0400

    added font and updated sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import { Routes,Route } from 'react-router-dom';
+import Layout from './assets/components/Layout';
+import './App.scss'
 function App(){
   return(
   <>
-  <h1>
-    this is the app
-  </h1>
+    <Routes>
+      <Route path="/" element={<Layout />}/>
+    </Routes>
   </>
   )
 }
---
+++ src/assets/components/Sidebar/index.jsx
@@ -0,0 +1,57 @@
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss'
+import { FaReact } from "react-icons/fa";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+function Sidebar() {
+    return(
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <FaReact className='icon'/>
+                <p>Sammy</p>
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color='#224870'/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about" to="/About">
+                    <FontAwesomeIcon icon={faUser} color='#224870'/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact" to="/Contact">
+                    <FontAwesomeIcon icon={faEnvelope} color='#224870'/>
+                </NavLink>
+            </nav>
+            <ul className='socials'>
+                <li>
+                    <a 
+                    href='https://www.linkedin.com/in/sammy-yang29/'
+                    rel='norefferer'
+                    target='_blank'
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} color='#224870'/>
+                    </a>
+                </li>
+                <li>
+                    <a 
+                    href='github.com/Ezimy/'
+                    rel='norefferer'
+                    target='_blank'
+                    >
+                        <FontAwesomeIcon icon={faGithub} color='#224870'/>
+                    </a>
+                </li>
+                <li>
+                    <a 
+                    href='www.youtube.com/@sammyyang5549'
+                    rel='norefferer'
+                    target='_blank'
+                    >
+                        <FontAwesomeIcon icon={faYoutube} color='#224870'/>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default Sidebar;

69cfe2b0e9fac66affd0252c0015a63136d2b954
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-05-07 17:18:23 -0400

    typewriter effect for text added

--- src/assets/components/Home/index.jsx
+++ src/assets/components/Home/index.jsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom';
 import './index.scss'
+import Typewriter from "typewriter-effect"
 function Home(){
+
     return(
         <>
         <div className='container home-page'>
             <div className='text-zone'>
                 <h1>
                     <br/>
-                    Hi, 
-                    <br/> I'm
-                    Sammy,
-                    <br/>
-                    A Web Developer
-                    <br/>
+                    <Typewriter
+                        onInit={(typewriter) => {
+                        typewriter
+                            .typeString("Hi, <br/> I'm Sammy <br/> A Web Developer")
+                            .pauseFor(1000)
+                            .start();
+                        }}
+                    />
                 </h1>
                 <h2>
                     Frontend Developer/React.js Expert/ JavaScript Expert
